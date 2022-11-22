12e1ea0ace657d0df6125e949b620e3ee7e83d23
Joanmarie <joanmarie@dartmouth.edu>
2022-11-21 21:20:33 -0500

    cleanup

---
+++ README.md
@@ -0,0 +1,8 @@
+
+So begins the **Py**thon **Co**mputational **G**eomorphology **S**oftware **S**ystem!
+
+Steps:
+1. Use the GEE Code Editor and `gee_scripts` to grab ya imagery
+2. Use `make_jpges.py` to turn ya Earth Engine files into jpgs for Doodler. 
+3. Use Dash Doodler to label/segment ya imagery
+4. Use Segmentation Gym to segment unlabeled imagery 
---
+++ watershed_files/random_state_0/make_jpgs_dev.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from osgeo import gdal
+import os
+import glob, os, shutil
+
+from pathlib import Path 
+
+#geotiff_dir = Path('./geotiffs')
+geotiff_dir = Path('.')
+jpg_dir = Path('./jpgs')
+jpg_dir.mkdir(exist_ok=True)
+
+
+if os.getlogin( ) == 'f0055wb':
+    doodler_asset_dir = Path('//dartfs-hpc/rc/home/b/f0055wb/dash_doodler/assets')
+elif os.getlogin( ) == 'f005dv1':
+    doodler_asset_dir = Path('//dartfs-hpc/rc/home/1/f005dv1/dash_doodler_dev/dash_doodler/assets')
+else:
+    print("WHo are you, go set your assets path")
+print("Sending files to: " + str(doodler_asset_dir))
+
+for tif_pth in geotiff_dir.glob('*_rgb*.tif'):
+    options_list = [
+        '-ot Byte',
+        '-of JPEG',
+        '-b 1',
+        '-b 2',
+        '-b 3'
+        '-scale'
+    ]           
+    
+    options_string = " ".join(options_list)
+
+    gdal.Translate(
+        (jpg_dir / f'{tif_pth.stem}.jpg').as_posix(),
+        tif_pth.as_posix(),
+        options=options_string
+    )
+    
+    #https://stackoverflow.com/questions/50207292/how-to-convert-geotiff-to-jpg-in-python-or-java
+    
+# Now move to the Doodler asset folder
+for pth in jpg_dir.glob('*_rgb*.jpg'):
+    shutil.copy2(pth, doodler_asset_dir / pth.name)
